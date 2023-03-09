97650ba5a707dc54a2a919e797c7a3c27cc2c065
PhilipV1 <philipvelandria@gmail.com>
2023-03-09 13:56:33 +0100

    Added enum menu options for better readability

--- ass3_queries.sql
+++ ass3_queries.sql
@@ -7,5 +7,7 @@ select * from members;
 alter table members
 add constraint UQ_email UNIQUE(email);
 
-insert into members
-VALUES ('Philip', 'Velandria', 'Stallvägen', 'Växjö', 'Kronoberg', '35256', '07638727', 'philipvelandria@gmail.com', '12345', none, none); 
+insert into members (fname, lname, address, city, state, zip, phone, email, password)
+VALUES ('Philip', 'Velandria', 'Stallvägen', 'Växjö', 'Kronoberg', '35256', '07638727', 'philipvelandria@gmail.com', '12345'); 
+
+show columns from members;
--- bookstore.py
+++ bookstore.py
@@ -1,66 +1,84 @@
 from getpass import getpass
 from mysql.connector import connect, Error
+from enum import Enum
+
+
+class Menu(Enum):
+    """Easier readability for menu options"""
+    MAIN = 0
+    USER = 1
+    NEWMEMBER = 2
+    QUIT = 3
+
 
 def main():
     run = True
-    userin = '0'
+    userin = Menu.MAIN
     sqlConnector = ConnectDB()
-    
+
     while run:
-        if userin == '0':
-            MenuType(0)
-            userin = UserInput()
-        if userin == '1':
-            MenuType(1)
-            userin = '0'
-        if userin == "2":
-            NewMemberMenu(sqlConnector)
-            input("Press enter to return to the main menu")
-            userin = '0'
-        if userin == "3":
-            PrintMembers(sqlConnector)
-            input("Press enter to return to the main menu")
-            userin = "0"
-        if userin.lower() == 'q':
+        if userin == Menu.MAIN.value:
+            menuType(Menu.MAIN.value)
+            userin = userInput()
+        if userin == Menu.USER.value:
+            menuType(Menu.USER.value)
+            userin = Menu.MAIN.value
+        if userin == Menu.NEWMEMBER.value:
+            newMemberMenu(sqlConnector)
+            input("\nPress enter to return to the main menu")
+            userin = Menu.MAIN.value
+        if userin == Menu.QUIT.value:
             run = False
         else:
-            userin = '0'
+            userin = Menu.MAIN.value
+
     sqlConnector.close()
 
+
 def ConnectDB():
-    
+
     try:
         account = input("Enter username: ")
         pword = getpass("Enter password: ")
         connection = connect(
-            host = "localhost",
-            user = account,
-            password = pword,
-            database = "book_store",
-        ) 
+            host="localhost",
+            user=account,
+            password=pword,
+            database="book_store",
+        )
     except Error as e:
-        print("Error generated message: " + e.msg)
+        print("Failed to connect to database: " + e.msg)
     return connection
 
-def MenuType(menuType=0):
+
+def menuType(menu=Menu.MAIN.value):
     line1 = "*******************************************\n"
     line2 = "***                                     ***\n"
     line3 = "***   Welcome to the Online Bookstore   ***\n"
-    if menuType == 0:
-        print(line1 + line2+ line3+ line2+ line1)
-        MainMenuOption()
-    if menuType == 1:
+    if menu == Menu.MAIN.value:
+        print(line1 + line2 + line3 + line2 + line1)
+        mainMenuOption()
+    if menu == Menu.USER.value:
         line4 = "***            Member Menu              ***\n"
         print(line1 + line2 + line3 + line4 + line2 + line1)
-        
-    
-def MainMenuOption():
+
+
+def memberMenuOption():
+    print("\t1. Browse by Subject")
+    print("\t2. Search by Author/Title")
+    print("\t3. Check out")
+    print("\t4. Logout")
+
+
+def mainMenuOption():
     print("\t1. Member login")
     print("\t2. New Member Registration")
     print("\t3. Show all members")
     print("\tQ. Quit\n")
 
-def NewMemberMenu(sqlConnector):
+
+def newMemberMenu(sqlConnector):
+    """Inserts a new member into the database using SQL queries"""
     print(sqlConnector)
     print("New member registration\n")
 
@@ -71,28 +89,30 @@ def NewMemberMenu(sqlConnector):
     state = input("Enter state: ")
     try:
         zipCode = int(input("Enter zip: "))
-    except:
-        print("Please enter a valid zip code")
+    except ValueError as e:
+        print(f"Not a valid zip code ERROR: {e}")
     phone = input("Enter phone number: ")
     email = input("Enter email address: ")
-    userID = input("Enter userID: ")
     password = input("Enter password: ")
 
     valueString = f"""
-    INSERT INTO members (fname, lname, address, city, state, zip, phone, email, password)
+    INSERT INTO members
+    (fname, lname, address, city, state, zip, phone, email, password)
     VALUES
-    ("{fname}", "{lname}", "{staddress}", "{city}", "{state}", {zipCode}, "{phone}", "{email}", "{password}")        
+    ("{fname}", "{lname}", "{staddress}", "{city}", "{state}",
+    {zipCode}, "{phone}", "{email}", "{password}")
     """
-    
-    with sqlConnector.cursor() as cursor:
-        cursor.execute(valueString)
-        sqlConnector.commit()
+    # Handle error if the user tries to enter a duplicate user
+    try:
+        with sqlConnector.cursor() as cursor:
+            cursor.execute(valueString)
+            sqlConnector.commit()
+            print("You have registered successfully")
+    except Error as e:
+        print(f"Failed to created new member: {e.msg}\n")
 
 
-    print("You have registered successfully")
-
-
-def PrintMembers(sqlConnector):
+def printMembers(sqlConnector):
     selectMembers = "SELECT * FROM members"
     with sqlConnector.cursor() as cursor:
         cursor.execute(selectMembers)
@@ -100,20 +120,23 @@ def PrintMembers(sqlConnector):
         for row in result:
             print(row)
 
-def UserInput():
-    validInputs = ["1", "2", "3", "q"];
-    try:
-        option = False
-        while not option:
-            value = str(input("Type in your option: "))
-            if validInputs.count(value.lower()) < 1:
+
+def userInput():
+    validInputs = [1, 2, 3, 4]
+
+    option = False
+    while not option:
+        try:
+            value = int(input("Type in your option: ")).lower()
+            ordValue = ord(value)
+            print(f"ASCII value = {ordValue}")
+            if validInputs.count(value) < 1:
                 print("Please choose from the appropriate options")
             else:
                 option = True
-    except:
-        print("Please enter a valid input")
+        except ValueError as e:
+            print(f"Invalid input: {e}")
     return value
-
 
 
 if __name__ == "__main__":
